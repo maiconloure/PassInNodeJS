eb196a94ecdf4d5ad51a623161826b8f88d894be
Maicon Lourenço <maiconloure@gmail.com>
2024-04-28 14:33:32 -0300

    feat: added new routes and check-in

--- src/routes/get-attendee-badge.ts
+++ src/routes/get-attendee-badge.ts
@@ -14,7 +14,14 @@ export async function getAttendeeBadge(app: FastifyInstance) {
             attendeeId: z.coerce.number().int(),
           }),
           response: {
-            
+            200: z.object({
+              badge: z.object({
+                name: z.string(),
+                email: z.string().email(),
+                eventTitle: z.string(),
+                checkInURL: z.string().url(),
+              }),
+            }),
           },
         },
       },
@@ -40,7 +47,18 @@ export async function getAttendeeBadge(app: FastifyInstance) {
           throw new Error('Attendee not found.')
         }
 
-        return reply.send({ attendee })
+        const baseURL = request.protocol + '://' + request.hostname
+
+        const checkInURL = new URL(`/attendees/${attendeeId}/check-in`, baseURL)
+
+        return reply.send({ 
+          badge: {
+            name: attendee.name,
+            email: attendee.email,
+            eventTitle: attendee.event.title,
+            checkInURL: checkInURL.toString(),
+          }
+         })
       }
     );
 }
--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,34 @@
 import fastify from "fastify";
-import { serializerCompiler, validatorCompiler } from "fastify-type-provider-zod";
+
+import fastifySwagger from "@fastify/swagger";
+import fastifySwaggerUI from "@fastify/swagger-ui";
+
+import { jsonSchemaTransform, serializerCompiler, validatorCompiler } from "fastify-type-provider-zod";
 import { createEvent } from "./routes/create-event";
 import { registerForEvent } from "./routes/register-for-event";
 import { getEvent } from "./routes/get-event";
 import { getAttendeeBadge } from "./routes/get-attendee-badge";
+import { checkIn } from "./routes/check-in";
+import { getEventAttendees } from "./routes/get-event-attendees";
 
 const app = fastify();
+
+app.register(fastifySwagger, {
+    swagger: {
+        consumes: ['application/json'],
+        produces: ['application/json'],
+        info: {
+            title: 'pass.in',
+            description: 'An API for event registration',
+            version: '1.0.0'
+        }
+    },
+    transform: jsonSchemaTransform
+});
+
+app.register(fastifySwaggerUI, {
+    routePrefix: '/docs'
+});
 
 // Add schema validator and serializer
 app.setValidatorCompiler(validatorCompiler);
@@ -15,6 +38,8 @@ app.register(createEvent)
 app.register(registerForEvent)
 app.register(getEvent)
 app.register(getAttendeeBadge)
+app.register(checkIn)
+app.register(getEventAttendees)
 
 app.listen({ port: 3333}).then(() => {
     console.log("HTTP Server running!");
