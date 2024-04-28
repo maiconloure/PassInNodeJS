e14786547e928a99e5e85c734138e8041b9fea9c
Maicon Lourenço <maiconloure@gmail.com>
2024-04-27 23:04:39 -0300

    feat: added create-event ad register-for-event

---
+++ src/server.ts
@@ -0,0 +1,17 @@
+import fastify from "fastify";
+import { serializerCompiler, validatorCompiler } from "fastify-type-provider-zod";
+import { createEvent } from "./routes/create-event";
+import { registerForEvent } from "./routes/register-for-event";
+
+const app = fastify();
+
+// Add schema validator and serializer
+app.setValidatorCompiler(validatorCompiler);
+app.setSerializerCompiler(serializerCompiler);
+
+app.register(createEvent)
+app.register(registerForEvent)
+
+app.listen({ port: 3333}).then(() => {
+    console.log("HTTP Server running!");
+})
